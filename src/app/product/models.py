81a937ea6fc03525db59edc0c7c5e865ab760d0a
javadhoseinzade <javadhoseinzde80@gmail.com>
2026-04-29 22:35:08 +0330

    add category model

--- src/app/product/models.py
+++ src/app/product/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.text import slugify
+from app.common.models import BaseModel
 
-# Create your models here.
+class Category(BaseModel):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=220, unique=True)
+
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True, blank=True,
+        related_name='subcategories'
+    )
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ['parent__id', 'title']
+
+    def __str__(self):
+        full_path = [self.title]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k = k.parent
+        return ' -> '.join(reversed(full_path))
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
