0c3cb7049b9d8f643be630da8f3917cda0ec7625
javadhoseinzade <javadhoseinzde80@gmail.com>
2026-05-06 23:08:39 +0330

    add CartSerializer

--- src/app/cart/serializer.py
+++ src/app/cart/serializer.py
@@ -12,3 +12,11 @@ class CartItemSerializer(serializers.ModelSerializer):
 
     def get_total_price(self, obj):
         return obj.total_price
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+    total_price = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'items', 'total_price']
