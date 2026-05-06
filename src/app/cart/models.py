299158f6167137be280e7b4adc807b4441876361
javadhoseinzade <javadhoseinzde80@gmail.com>
2026-05-06 23:03:55 +0330

    add CartItem models

--- src/app/cart/models.py
+++ src/app/cart/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from app.common.models import BaseModel
+from app.product.models import Product
 
 class Cart(BaseModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
@@ -11,3 +12,15 @@ class Cart(BaseModel):
     @property
     def total_price(self):
         return sum(item.total_price for item in self.items.all())
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name="items",on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.product.title
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
