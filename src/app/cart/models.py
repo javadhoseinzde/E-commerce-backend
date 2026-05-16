2919276006705f09d28d855ca0b9a1f7b2e7c657
javadhoseinzade <javadhoseinzde80@gmail.com>
2026-05-16 19:55:29 +0330

    add variant to CartItem

--- src/app/cart/models.py
+++ src/app/cart/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from app.common.models import BaseModel
-from app.product.models import Product
+from app.product.models import Product, ProductVariant
 
 class Cart(BaseModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
@@ -17,6 +17,7 @@ class Cart(BaseModel):
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, related_name="items",on_delete=models.CASCADE)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    variant = models.ForeignKey(ProductVariant, on_delete=models.SET_NULL, null=True, blank=True, related_name='cart_items')
     quantity = models.PositiveIntegerField(default=1)
     is_ordered = models.BooleanField(default=False)
     def __str__(self):
